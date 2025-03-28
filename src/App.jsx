b8288f53c6e8a58e49c202686d62b326afd61cb2
Mohammad <razamohammad0589@gmail.com>
2025-03-28 17:17:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import First from './component/First'
+
+const App = () => {
+  return (
+    <div className='h-10'>
+      <First name="raza"/>
+      <First name="tabish"/>
+      <First name="alam"/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/First.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const First = (props) => {
+  return (
+    <div className='h-10 bg-gray-600 p-8 text-white '>
+      {props.name}
+    </div>
+  )
+}
+
+export default First
+
